bd7d5b722ed63411a8a0bfb56627494aa91393c4
Leedje <leedje2006@hotmail.com>
2024-05-08 10:55:52 +0200

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,26 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorApp.Client;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace Webcam_Robot_WebAssembly
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+            await builder.Build().RunAsync();
 
-await builder.Build().RunAsync();
+            // Configure HttpClient to send requests to your Arduino
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                //Voer IP Adres in
+                BaseAddress = new Uri("http://145.93.232.26/") // Replace with your actual Arduino IP
+            });
+
+        }
+
+    }
+}
